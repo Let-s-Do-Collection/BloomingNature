4f3f8e7d5eac9c0de6c21b9f22c5ca997edcad41
Ninjdai <ninjdai@duck.com>
2025-05-16 10:14:39 +0200

    feat: replace hardcoded forest moss spread with data-driven configured feature

--- common/src/main/java/net/satisfy/bloomingnature/core/block/ForestMossBlock.java
+++ common/src/main/java/net/satisfy/bloomingnature/core/block/ForestMossBlock.java
@@ -1,6 +1,7 @@
 package net.satisfy.bloomingnature.core.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
@@ -12,6 +13,7 @@ import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.MudBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.satisfy.bloomingnature.core.registry.ObjectRegistry;
+import net.satisfy.bloomingnature.core.world.ConfiguredFeatures;
 
 public class ForestMossBlock extends MudBlock implements BonemealableBlock {
     public ForestMossBlock(Properties properties) {
@@ -27,26 +29,7 @@ public class ForestMossBlock extends MudBlock implements BonemealableBlock {
     }
 
     public void performBonemeal(ServerLevel serverLevel, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
-        for (int i = 0; i < 3; i++) {
-            BlockPos pos = blockPos.offset(randomSource.nextInt(3) - 1, 0, randomSource.nextInt(3) - 1);
-            if (serverLevel.getBlockState(pos).isAir()) {
-                BlockState toSpawn;
-                switch (randomSource.nextInt(3)) {
-                    case 0:
-                        toSpawn = ObjectRegistry.FOREST_MOSS_CARPET.get().defaultBlockState();
-                        break;
-                    case 1:
-                        toSpawn = ObjectRegistry.MOSSGRASS.get().defaultBlockState();
-                        break;
-                    case 2:
-                        toSpawn = Blocks.BROWN_MUSHROOM.defaultBlockState();
-                        break;
-                    default:
-                        continue;
-                }
-                serverLevel.setBlock(pos, toSpawn, 3);
-            }
-        }
+        serverLevel.registryAccess().registry(Registries.CONFIGURED_FEATURE).flatMap((registry) -> registry.getHolder(ConfiguredFeatures.FOREST_MOSS_PATCH_BONEMEAL_KEY)).ifPresent((reference) -> reference.value().place(serverLevel, serverLevel.getChunkSource().getGenerator(), randomSource, blockPos.above()));
     }
 
     @Override
--- common/src/main/java/net/satisfy/bloomingnature/core/world/ConfiguredFeatures.java
+++ common/src/main/java/net/satisfy/bloomingnature/core/world/ConfiguredFeatures.java
@@ -7,6 +7,7 @@ import net.satisfy.bloomingnature.core.util.BloomingNatureIdentifier;
 
 public class ConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> FAN_PALM_TREE_KEY = registerKey("beach_fan_palm");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> FOREST_MOSS_PATCH_BONEMEAL_KEY = registerKey("forest_moss_patch_bonemeal");
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
         return ResourceKey.create(Registries.CONFIGURED_FEATURE, new BloomingNatureIdentifier(name));
