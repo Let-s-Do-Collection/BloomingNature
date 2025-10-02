c6bdb7c24166f613a1fc16d38ce95778bc75c854
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2025-10-02 10:11:30 +0200

    feat: replace hardcoded forest moss spread with data-driven configured feature (#21)
    Co-authored-by: mrclb - Marco <104036234+satisfyu@users.noreply.github.com>

--- common/src/main/java/net/satisfy/bloomingnature/core/world/ConfiguredFeatures.java
+++ common/src/main/java/net/satisfy/bloomingnature/core/world/ConfiguredFeatures.java
@@ -6,7 +6,10 @@ import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.satisfy.bloomingnature.BloomingNature;
 
 public class ConfiguredFeatures {
-    public static final ResourceKey<ConfiguredFeature<?,?>> FAN_PALM_TREE_KEY = registerKey("beach_fan_palm");
+
+    public static final ResourceKey<ConfiguredFeature<?, ?>> FAN_PALM_TREE_KEY = registerKey("beach_fan_palm");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> FOREST_MOSS_PATCH_BONEMEAL_KEY = registerKey("forest_moss_patch_bonemeal");
+
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
         return ResourceKey.create(Registries.CONFIGURED_FEATURE, BloomingNature.identifier(name));
